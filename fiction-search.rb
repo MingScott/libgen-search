2893c3cea25bf1e3837da5e0dd12106914d929c6
Ming Scott <ming.scott@gmail.com>
2019-10-15 23:39:50 -0300

    Basic functionality working!

--- fiction-search.rb
+++ fiction-search.rb
@@ -1,6 +1,8 @@
+#!/usr/bin/env ruby
 require 'nokogiri'
 require 'open-uri'
 require 'optparse'
+require 'net/http'
 
 # http://gen.lib.rus.ec/fiction/?q=watership+down&criteria=title&language=English&format=mobi
 crit = ""
@@ -34,6 +36,29 @@ end.parse!
 query = ARGV.join("+")
 @url = "http://gen.lib.rus.ec/fiction/?q=#{query}&criteria=#{crit}#{wildcard}&language=#{lang}&format=#{form}"
 search_page = Nokogiri::HTML(open(@url))
-results = search_page.css("tr td a")
-puts results.to_s
-puts search_page.to_s
+results = {}
+results[:titles] = []
+results[:authors] = []
+results[:files] = []
+links = search_page.css("tr td a")
+links.each do |c|
+	unless results[:titles].include?(c.content)
+		results[:titles] << c.content unless ( c.content[0] == "[" and c.content[2] == "]" ) or c.keys.include?("title")
+		results[:authors] << c.content if c["title"].include?("search") if c.keys.include?("title")
+		tmpfile = c["href"] if c.content == "[1]"
+		if tmpfile.class == String
+			doc = Nokogiri::HTML(open(tmpfile))
+			results[:files] << "http://#{tmpfile.split("/")[2]}#{doc.css("a").first["href"]}"
+		end
+	end
+end
+puts "Saving first of each unique title matching search:"
+for ii in 0..results[:titles].length-1
+	puts "#{results[:authors][ii]}	#{results[:titles][ii]}"
+	fname = "data/#{results[:titles][ii].gsub(" ","_")}.#{form}"
+	File.open(fname, "wb") do |local_f|
+		open(results[:files][ii], 'rb') do |remote_f|
+			local_f.write(remote_f.read)
+		end
+	end
+end
