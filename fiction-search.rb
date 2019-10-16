c39e10c421fd3311563720c5599ecf58b1e2cb34
Ming Scott <ming.scott@gmail.com>
2019-10-16 01:24:51 -0300

    Pretty much fully working! Add to github tomorrow

--- fiction-search.rb
+++ fiction-search.rb
@@ -10,6 +10,8 @@ lang = "English"
 form = "mobi"
 download = false
 view_results = true
+view_download_links = true
+unique = true
 OptionParser.new do |o|
 	o.banner = ""
 	o.on("-a","--crit-author", "search by author") do |a|
@@ -36,25 +38,33 @@ OptionParser.new do |o|
 	o.on("-D", "--download", "Download all unique books") do |d|
 		download = true
 	end
-	o.on("-x", "--hide-results", "do not view results") do |v|
+	o.on("--hide-results", "do not view results") do |v|
 		view_results = false
+	end
+	o.on("--hide-links", "do not view download links with results") do |l|
+		view_download_links = false
+	end
+	o.on("-u","--show-all", "Show all results") do |x|
+		unique = false
 	end
 end.parse!
 query = ARGV.join("+")
 base_url = "http://gen.lib.rus.ec"
 @url = "http://gen.lib.rus.ec/fiction/?q=#{query}&criteria=#{crit}#{wildcard}&language=#{lang}&format=#{form}"
+puts "query:	#{@url}\n\n"
 
 results = {}
 results[:titles] = []
 results[:authors] = []
 results[:files] = []
+results[:format] = []
 
 next_page = @url
 while next_page
 	search_page = Nokogiri::HTML(open(next_page))
 	links = search_page.css("tr td a")
 	links.each do |c|
-		unless results[:titles].include?(c.content)
+		unless results[:titles].include?(c.content) and unique
 			results[:titles] << c.content unless ( c.content[0] == "[" and c.content[2] == "]" ) or c.keys.include?("title")
 			results[:authors] << c.content if c["title"].include?("search") if c.keys.include?("title")
 			tmpfile = c["href"] if c.content == "[1]"
@@ -73,9 +83,11 @@ while next_page
 	end
 	next_page = false if next_page == old_page
 end
-puts "Saving first of each unique title matching search:" if view_results
+
 for ii in 0..results[:titles].length-1
-	puts "#{results[:authors][ii]}\n#{results[:titles][ii]}\n" if view_results
+	link = ""
+	link = results[:files][ii] if view_download_links
+	puts "#{(ii+1).to_s}:		#{results[:authors][ii]}\n	#{results[:titles][ii]}\n#{link}\n\n" if view_results
 	fname = "#{results[:titles][ii].gsub(" ","_")}.#{form}"
 	File.open(fname, "wb") do |local_f|
 		open(results[:files][ii], 'rb') do |remote_f|
